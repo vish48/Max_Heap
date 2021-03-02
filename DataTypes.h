da112f3c5d9d5daa6e37b3566c1891b8605ffede
Nicholas <nicosmith.smith3@gmail.com>
2021-03-02 19:39:07 -0400

    added comments to the structs

--- DataTypes.h
+++ DataTypes.h
@@ -4,31 +4,33 @@
 #include <iostream>
 using namespace std;
 
-//Definitions of structs that will be used in the .cpp files
+//DEFINITIONS OF STRUCTS THAT WILL BE USED IN THE .CPP FILES
+//A STRUCT CONTAINING CONDITIONS AND PRIORITUES WHICH ARE STORED IN A HASHTABLE
 struct Condition {
 	string conditionName;
 	int priority;
 };
-
+//A STRUCT CONTAINING PATIENT INFORMATION WHICH IS STORED IN THE PATIENT DATABASE(BINARY SEARCH TREE) 
 struct Patient {
 	int patientID;
 	string patientName;
 	string phone;
 };
-
+//A STRUCT CONTAINING INFORMATION ABOUT A PATIENT TO BE TREATED. THE MAX PRIORITY QUEUE AND THE QUEUE OF
+//PATIENTS ALREADY TREATED CONTAIN DATA TYPE INCIDENT.
 struct Incident {
 	int patientID;
 	string condition;
 	int priority;
 };
-   
+//A NODE IN THE BINARY SEARCH TREE (WHICH STORES DATA OF TYPE PATIENT)
 struct BTNode {
 	Patient patient;
 	BTNode * left;
 	BTNode * right;
 	BTNode * parent;
 };
-
+//A NODE IN THE QUEUE OF PATIENTS ALREADY TREATED
 struct QueueNode {
 	Incident data;
 	QueueNode * next;	
